06908bf68d9fba138d9ce8b6a7efe642f962130b
j1kka <zhyldyz.kanatbekova@alatoo.edu.kg>
2024-11-10 15:47:36 +0600

    Create fakeTranslate.js

---
+++ fakeTranslate.js
@@ -0,0 +1,38 @@
+const dictionary = {
+    "hello": "здравствуйте",
+    "world": "мир",
+    "friend": "друг",
+    "good": "хорошо",
+    "day": "день",
+    "night": "ночь",
+    "love": "любовь",
+    "peace": "мир",
+    "cat": "кот",
+    "dog": "собака",
+    "water": "вода",
+    "fire": "огонь",
+    "earth": "земля",
+    "wind": "ветер",
+    "sun": "солнце",
+    "moon": "луна",
+    "tree": "дерево",
+    "flower": "цветок",
+    "book": "книга",
+    "computer": "компьютер"
+};
+
+function fakeTranslate(word) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const translation = dictionary[word.toLowerCase()];
+            if (translation) {
+                resolve(translation);
+            } else {
+                reject("Translation not found.");
+            }
+        }, 1000); 
+    });
+}
+
+export { fakeTranslate };
+
